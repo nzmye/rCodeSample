ecef5fe150bcb7a0c093d1bea89f8fa5c4395f71
nzmye <nzmye.arslan01@gmail.com>
2018-09-20 17:27:40 +0300

    get random number and adding some general practice

--- test.R
+++ test.R
@@ -1,6 +1,7 @@
 
 countdown <- function(from)
 {
+  # browser() ## for debugging
   print(from)
   while (from != 0) {
     Sys.sleep(1)
@@ -9,9 +10,35 @@ countdown <- function(from)
   }
 }
 
- countdown(8) 
+getrandomnumber <- function()
+{
+  #browser()
+  num <- round(runif(1)*100, digits = 0)  # get random number
+  return(as.integer(num))
+}
 
- 
- 
- 
+readinteger <- function()
+{
+  n <- readline(prompt = "Enter an integer: ")  #getting user input
+  if(!grepl("^[0-9]+$",n))  # integer control
+  {
+    cat("please check you entered an integer number")  #print warning
+    return(readinteger())
+  }
+  return(as.integer(n))
+}
+
+
+countdown(getrandomnumber()) 
+
+
+#general practice
+sum(0:9)
+append(LETTERS[1:13],letters[14:26]) 
+c(1,4,5,8)*2 
+something <- c(1,4,letters[2])
+length(something)
+
+
+
  
